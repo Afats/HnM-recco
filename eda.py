a7e4d367bbec28de1d5771d4058d745dc44c816d
Afats <mmdohad@gmail.com>
2023-04-05 20:38:57 +1000

    work moved to Kaggle notebook. See readme file.

--- eda.py
+++ eda.py
@@ -11,41 +11,37 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # Importing data
-article_df = pd.read_csv('articles.csv')
+articles = pd.read_csv('articles.csv')
 # print(article_df.head())
-customer_df = pd.read_csv('customers.csv')
+customers = pd.read_csv('customers.csv')
 # print(customer_df.head())
-
-# Data pre-proccessing -------------
+transactions = pd.read_csv('transactions.csv')
 
 # ----- empty value stats -------------
 
-# print out the number of missing values in each column
-print(customer_df.isnull().sum())
+print("Missing values: ")
+print(customers.isnull().sum())
+print("--")
 
-
-print("FN Newsletter vals: ", customer_df['FN'].unique())
-print("Active communication vals: ",customer_df['Active'].unique())
-print("Club member status vals: ", customer_df['club_member_status'].unique())
-print("Fashion News frequency vals: ", customer_df['fashion_news_frequency'].unique())
-# print("Age vals: ", customer_df['age'].unique())
+print("FN Newsletter vals: ", customers['FN'].unique())
+print("Active communication vals: ",customers['Active'].unique())
+print("Club member status vals: ", customers['club_member_status'].unique())
+print("Fashion News frequency vals: ", customers['fashion_news_frequency'].unique())
+# print("Age vals: ", customers['age'].unique())
+print("--")
 
 # ---- data cleaning -------------
-customer_df['FN'] = customer_df['FN'].fillna(0.0)
-customer_df['Active'] = customer_df['Active'].fillna(0)
+customers['FN'] = customers['FN'].fillna(0.0)
+customers['Active'] = customers['Active'].fillna(0)
 
 # replace club_member_status missing values with 'PRE-CREATE' (assuming that the customer has not yet created an account)
-customer_df['club_member_status'] = customer_df['club_member_status'].fillna('PRE-CREATE')
-customer_df['fashion_news_frequency'] = customer_df['fashion_news_frequency'].fillna('None')
-customer_df['fashion_news_frequency'] = customer_df['fashion_news_frequency'].replace('NONE', 'none')
-customer_df['age'] = customer_df['age'].fillna(customer_df['age'].mean())
-customer_df['age'] = customer_df['age'].astype(int)
-article_df['detail_desc'] = article_df['detail_desc'].fillna('None')
+customers['club_member_status'] = customers['club_member_status'].fillna('PRE-CREATE')
+customers['fashion_news_frequency'] = customers['fashion_news_frequency'].fillna('None')
+customers['fashion_news_frequency'] = customers['fashion_news_frequency'].replace('NONE', 'none')
+customers['age'] = customers['age'].fillna(customers['age'].mean())
+customers['age'] = customers['age'].astype(int)
+articles['detail_desc'] = articles['detail_desc'].fillna('None')
 
-print(customer_df.isnull().sum())
-print(article_df.isnull().sum())
-
-
-
-
-
+print("Missing values: ")
+print(customers.isnull().sum())
+print("--")
