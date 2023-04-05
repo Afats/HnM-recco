360145b1b9a72546a460cde7fb3dfa606398c703
Afats <mmdohad@gmail.com>
2023-04-05 14:50:37 +1000

    missing values filles

--- eda.py
+++ eda.py
@@ -11,16 +11,36 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # Importing data
-
 article_df = pd.read_csv('articles.csv')
-print(article_df.head())
-
+# print(article_df.head())
 customer_df = pd.read_csv('customers.csv')
-print(customer_df.head())
+# print(customer_df.head())
 
-# Data preprocessing
+# Data pre-proccessing -------------
 
-# fill out missing values in customer_df
+# ----- empty value stats -------------
+
+# print out the number of missing values in each column
+print(customer_df.isnull().sum())
+
+
+print("FN Newsletter vals: ", customer_df['FN'].unique())
+print("Active communication vals: ",customer_df['Active'].unique())
+print("Club member status vals: ", customer_df['club_member_status'].unique())
+print("Fashion News frequency vals: ", customer_df['fashion_news_frequency'].unique())
+# print("Age vals: ", customer_df['age'].unique())
+
+# ---- data cleaning -------------
+customer_df['FN'] = customer_df['FN'].fillna(0.0)
+customer_df['Active'] = customer_df['Active'].fillna(0)
+
+# replace club_member_status missing values with 'PRE-CREATE' (assuming that the customer has not yet created an account)
+customer_df['club_member_status'] = customer_df['club_member_status'].fillna('PRE-CREATE')
+customer_df['fashion_news_frequency'] = customer_df['fashion_news_frequency'].fillna('None')
+customer_df['fashion_news_frequency'] = customer_df['fashion_news_frequency'].replace('NONE', 'none')
+customer_df['age'] = customer_df['age'].fillna(customer_df['age'].mean())
+
+print(customer_df.isnull().sum())
 
 
 
